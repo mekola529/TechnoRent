201e4034ae67342ff583a00137db52bce0f27660
mekola529 <gusirmikola@gmail.com>
2026-03-22 01:10:07 +0200

    fix: remove unused useCallback import

--- client/src/components/OrderModal.tsx
+++ client/src/components/OrderModal.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 import { createPortal } from "react-dom";
 import { createOrder } from "../data/equipment.service";
 
